20ebc39d37ad06043aeaf8879cb5ca36878c1fce
915_Nagy_Matyas <tomesjerry3@gmail.com>
2023-08-23 13:14:50 +0300

    Searchpage - nonfunctional search button

--- frontend/src/SearchPage.tsx
+++ frontend/src/SearchPage.tsx
@@ -1,7 +1,7 @@
 import { Fragment, useEffect, useState } from "react";
 import './SearchPage.css';
 import searchLogo from './images/Image 1@3x.png'
-import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, ToggleButton, ToggleButtonGroup, css } from "@mui/material";
+import { Button, Divider, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, ToggleButton, ToggleButtonGroup, css } from "@mui/material";
 
 const salePrices = ['75000', '100000', '125000', '150000', '175000', '200000', '225000', '250000', '275000', '300000', '325000', '350000', '375000', '400000', '425000', '450000', '475000', '500000', '600000', '700000', '800000', '900000', '1000000', '1250000', '1500000', '1750000', '2000000', '2250000', '2500000', '2750000', '3000000', '4000000', '5000000', '6000000', '7000000', '8000000', '9000000', '10000000'];
 const lettingPrices = ['400', '425', '450', '475', '500', '525', '550', '575', '600', '650', '700', '750', '800', '850', '900', '950', '1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700', '1800', '1900', '2000', '2250', '2500', '2750', '3000', '4000', '5000', '6000', '7500', '8000', '9000', '10000', '12500', '15000'];
@@ -81,6 +81,15 @@ const SearchPage = () => {
 		);
 	}
 
+	function handleSearchClick() {
+		console.log('Search clicked');
+		console.log('Transaction Type: ' + currentTransactionType);
+		console.log('Location: ' + currentLocation);
+		console.log('Price: ' + currentPrice);
+		console.log('Bedroom: ' + currentBedroom);
+		console.log('Property Type: ' + currentPropertyType);
+	}
+
 	function getLocationSelect() {
 		return (
 			<FormControl>
@@ -135,7 +144,7 @@ const SearchPage = () => {
 			<FormControl>
 				<InputLabel id="price-label">{priceLabel}</InputLabel>
 				<Select
-					sx={{ borderRadius: '50px', width: '80vw', marginBottom: '20px'  }}
+					sx={{ borderRadius: '50px', width: '80vw', marginBottom: '20px' }}
 					labelId="price-label"
 					id="price"
 					value={currentPrice}
@@ -149,7 +158,7 @@ const SearchPage = () => {
 					))}
 				</Select>
 			</FormControl>
-			)
+		)
 	}
 
 	function getBedroomSelect() {
@@ -157,7 +166,7 @@ const SearchPage = () => {
 			<FormControl>
 				<InputLabel id="bedroom-label">Bedrooms</InputLabel>
 				<Select
-					sx={{ borderRadius: '50px', width: '80vw', marginBottom: '20px'  }}
+					sx={{ borderRadius: '50px', width: '80vw', marginBottom: '20px' }}
 					labelId="bedroom-label"
 					id="bedroom"
 					value={currentBedroom}
@@ -174,6 +183,19 @@ const SearchPage = () => {
 		)
 	}
 
+	function getSearchButton() {
+		return (
+			<Fragment>
+
+				{/* <Divider sx={{ margin: '0 0 0 0', flexGrow: '1'  }} /> */}
+
+				<Button sx={{ borderRadius: '50px', width: '80vw', marginTop: '50px' }} variant="contained" onClick={handleSearchClick}>
+					Search
+				</Button>
+			</Fragment>
+		)
+	}
+
 	return (
 		<Fragment>
 			<div className="search-container">
@@ -182,10 +204,11 @@ const SearchPage = () => {
 					<ToggleButton sx={{ borderRadius: '50px', width: '40vw', height: '15px', marginBottom: '20px' }} value="sales">Sales</ToggleButton>
 					<ToggleButton sx={{ borderRadius: '50px', width: '40vw', height: '15px', marginBottom: '20px' }} value="lettings">Lettings</ToggleButton>
 				</ToggleButtonGroup>
-				{getTypeSelect()}
 				{getLocationSelect()}
 				{getPriceSelect()}
+				{getTypeSelect()}
 				{getBedroomSelect()}
+				{getSearchButton()}
 			</div>
 		</Fragment>
 	)
